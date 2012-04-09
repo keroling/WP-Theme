83f97dc2c493361a8c81bca1733985efa5ea908d
Touka Tachibana <aozora100+git@gmail.com>
2012-04-09 20:30:02 +0900

    ヘッダー改造ほか

--- twentyeleven_kai/functions.php
+++ twentyeleven_kai/functions.php
@@ -8,16 +8,19 @@
 
 /**
  * remove "<meta name="generator" content="WordPress X.X.X" />"
+ * WordPressのバージョン情報を隠す
  */
 remove_action('wp_head', 'wp_generator');
 
 /**
  * if you do not wish to deliver "wlwmanifest", you leave this uncommented.(remove // )
+ * wlwmanifestを出力しない（headｎにタグを埋めこませない）場合は”行頭の//”を削除する。
  */
 //remove_action('wp_head', 'wlwmanifest_link');
 
 /**
- * if you do not wish to deliver feeds, you leave　these uncommented.
+ * if you do not wish to deliver feeds, you leave　these uncommented.(remove // )
+ * ｆフィードを出力させない場合は行頭の”//”を削除する。
  */
 
 //remove_action('wp_head', 'rsd_link');
@@ -37,6 +40,13 @@ remove_action('wp_head', 'wp_generator');
 //remove_action('do_feed_atom', 'do_feed_atom', 10, 1);
 
 //automatic_feed_links(false);
+
+/**
+ * if you change size of your header image, you can change "1000 288".(& remove // )
+ * ヘッダー画像のサイズを変えるときは、下の数字をそのサイズに変更する。行頭の”//”も削除する。
+ */
+//define( 'HEADER_IMAGE_WIDTH', apply_filters( 'twentyeleven_header_image_width', 1000 ) );
+//define( 'HEADER_IMAGE_HEIGHT', apply_filters( 'twentyeleven_header_image_height', 288 ) );
 
 
 
